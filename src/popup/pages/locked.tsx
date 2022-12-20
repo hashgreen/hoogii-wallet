cefb4f561e5e2f70e488f7e1301ec33720965569
AhWeiHENGuEi <aa9440307@gmail.com>
2022-12-20 17:00:36 +0800

    chore: unlock wallet

--- src/popup/pages/locked.tsx
+++ src/popup/pages/locked.tsx
@@ -32,7 +32,12 @@ const Locked = () => {
                 onSubmit={async (e) => {
                     e.preventDefault()
                     const isValid = await checkPassword(password)
-                    if (!isValid) setError(t('error-password-incorrect'))
+                    if (!isValid) {
+                        setError(t('error-password-incorrect'))
+                    } else {
+                        // TODO: do something here or close windows
+                        window.close()
+                    }
                 }}
                 className="h-full pt-[116px] pb-12 flex-col-center scroll-di"
             >
