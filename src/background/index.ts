2a00c9e6569ac46cea99d065b19c0125c44851da
AhWeiHENGuEi <aa9440307@gmail.com>
2023-01-04 15:00:07 +0800

    feat: use define error message

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,3 +1,4 @@
+import * as Errors from '~/api/extension/errors'
 import { createPopup, createTab } from '~/api/extension/extension'
 import Messaging, { BackgroundController } from '~/api/extension/messaging'
 import { requestHandler } from '~/api/extension/request'
@@ -17,11 +18,7 @@ controller.add(MethodEnum.IS_VALID_WALLET, async (request, sendResponse) => {
     if (!keyring) {
         sendResponse({
             ...request,
-            data: {
-                error: true,
-                code: 401,
-                message: 'Please create a wallet first',
-            },
+            data: Errors.UnauthorizedError,
             sender: SenderEnum.EXTENSION,
             target: SenderEnum.WEBPAGE,
         })
@@ -172,13 +169,7 @@ const authHandler = async (request: IMessage<RequestArguments>) => {
 controller.add(MethodEnum.REQUEST, async (request, sendResponse) => {
     const auth = await authHandler(request)
 
-    const data = auth
-        ? await requestHandler(request)
-        : {
-              error: true,
-              code: 4002,
-              message: 'user rejected request',
-          }
+    const data = auth ? await requestHandler(request) : Errors.UnauthorizedError
 
     sendResponse({
         ...request,
