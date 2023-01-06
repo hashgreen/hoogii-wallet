d38414ac1a868301684597d0010b3180eee0d810
hryjosn <jo74705@gmail.com>
2023-01-06 16:24:22 +0800

    fix: remove ga

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import './index.scss'
 
 import { Buffer } from 'buffer'
 import ReactDOM from 'react-dom'
-import ReactGA from 'react-ga4'
 
 import Components from '~/container/Components'
 
@@ -12,11 +11,6 @@ import App from './App'
 
 window.global = window
 window.Buffer = Buffer
-const GA_ID = import.meta.env.VITE_GA4_MEASUREMENT_ID
-if (GA_ID) {
-    ReactGA.initialize(GA_ID)
-}
-
 ReactDOM.render(
     <>
         <App />
