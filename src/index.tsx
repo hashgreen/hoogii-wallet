4e2443b0a24a47ed7138fb88aef2efce6fcdb869
hryjosn <jo74705@gmail.com>
2023-01-07 15:20:17 +0800

    fix: fix address disappear

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import '~/utils/i18n'
 import './index.scss'
 
 import { Buffer } from 'buffer'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 
 // TODO: for dev in the future
 // import Components from '~/container/Components'
@@ -11,7 +11,7 @@ import App from './App'
 
 window.global = window
 window.Buffer = Buffer
-ReactDOM.render(
+ReactDOM.createRoot(
     <>
         <App />
         {/* <Components /> */}
