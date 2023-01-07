8c067267a692518b28fdd74822bab7b55c13329c
AhWeiHENGuEi <aa9440307@gmail.com>
2023-01-07 13:23:42 +0800

    fix: remove twice setStorage

--- src/store/WalletStore.ts
+++ src/store/WalletStore.ts
@@ -144,7 +144,7 @@ class WalletStore {
         if (!this.chain) return
 
         const puzzleHash = seedToPuzzle(seed).hashHex()
-        await setStorage({ puzzleHash })
+
         if (!(await getStorage<string>(StorageEnum.puzzleHash))) {
             await setStorage({ puzzleHash })
         }
