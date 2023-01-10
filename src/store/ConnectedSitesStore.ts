eb37d034511effbab54718efc703ff1c3cbb5272
AhWeiHENGuEi <aa9440307@gmail.com>
2023-01-10 17:17:34 +0800

    fix: use db replace walletStore db

--- src/store/ConnectedSitesStore.ts
+++ src/store/ConnectedSitesStore.ts
@@ -1,42 +1,19 @@
-import { liveQuery } from 'dexie'
-import { makeAutoObservable, onBecomeUnobserved } from 'mobx'
+import { makeAutoObservable } from 'mobx'
 
-import { IConnectedSite, WalletDexie } from '~/db'
+import { WalletDexie } from '~/db'
 import { ChainEnum } from '~/types/chia'
 import { StorageEnum } from '~/types/storage'
-import { getStorage } from '~/utils/storage'
+import { getStorage } from '~/utils/extension/storage'
 class ConnectedSitesStore {
-    connectedSites: IConnectedSite[] = []
-
-    unsubscribeConnectedSites = () => {}
-    subscribeConnectedSites = () => {
-        const observable = liveQuery(async () => {
-            const chainId = await getStorage<string>(StorageEnum.chainId)
-            const db = new WalletDexie(chainId ?? ChainEnum.Mainnet)
-            return db.connectedSites.toArray()
-        })
-        const subscription = observable.subscribe({
-            next: (result) => (this.connectedSites = result),
-        })
-        this.unsubscribeConnectedSites = () => subscription.unsubscribe()
-    }
-
     constructor() {
         makeAutoObservable(this)
-        this.subscribeConnectedSites()
-        onBecomeUnobserved(
-            this,
-            'connectedSites',
-            this.unsubscribeConnectedSites
-        )
     }
 
     isConnectedSite = async (origin) => {
         const chainId = await getStorage<string>(StorageEnum.chainId)
-        console.log('chainId', chainId)
-        // const db = new WalletDexie(chainId ?? ChainEnum.Mainnet)
-        // const connectedSites = db.connectedSites as IConnectedSite[]
-        return this.connectedSites.some((site) => site.url === origin)
+        const db = new WalletDexie(chainId ?? ChainEnum.Mainnet)
+        const connectedSites = await db.connectedSites.toArray()
+        return connectedSites.some((site) => site.url === origin)
     }
 }
 
