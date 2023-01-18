57ed4312b716c8b6b745870760083f0c4a69aafa
AhWeiHENGuEi <aa9440307@gmail.com>
2023-01-18 16:39:23 +0800

    fix: inject script

--- src/injected-scripts/index.ts
+++ src/injected-scripts/index.ts
@@ -1,23 +1,29 @@
 import { event, eventOff, isConnected, request } from '~/api/extension/webpage'
 
 import pkg from '../../package.json'
-window.chia = {
-    ...(window.chia || {}),
-    hoogii: {
-        name: pkg.name,
-        apiVersion: '1.0.0', // chip02 version
-        version: pkg.version,
-        isHoogii: true,
-        request: async (...arg) => {
-            return (await request(...arg))?.data
-        },
-        on: (eventName, callback) => {
-            event(eventName, callback)
-        },
-        off: (eventName, callback) => {
-            eventOff(eventName, callback)
-        },
-        isConnected: async () => (await isConnected()).data,
-        _events: {},
+
+const hoogii = {
+    name: pkg.name,
+    apiVersion: '1.0.0', // chip02 version
+    version: pkg.version,
+    isHoogii: true,
+    request: async (arg) => {
+        return (await request(arg))?.data
     },
+    on: (eventName, callback) => {
+        event(eventName, callback)
+    },
+    off: (eventName, callback) => {
+        eventOff(eventName, callback)
+    },
+    isConnected: async () => (await isConnected()).data,
+    _events: {},
+}
+
+if (window.chia) {
+    window.chia.hoogii = hoogii
+} else {
+    window.chia = {
+        hoogii,
+    }
 }
