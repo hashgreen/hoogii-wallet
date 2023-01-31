77a1f3b7b23c8ad5d41473a3156d7e84c7fcd5fe
AhWeiHENGuEi <aa9440307@gmail.com>
2023-01-31 15:23:26 +0800

    feat: add  announcement class

---
+++ src/utils/Announcement.ts
@@ -0,0 +1,28 @@
+import { concatBytes, fromHex, hash256 } from '@rigidity/bls-signatures'
+import { Program } from '@rigidity/clvm'
+
+class Announcement {
+    originInfo: string
+    message: Program
+    morphBytes?
+    constructor(originInfo, message, morphBytes?) {
+        this.originInfo = originInfo
+        this.message = message
+        this.morphBytes = morphBytes
+    }
+
+    name = (): Program => {
+        let message: Uint8Array = fromHex(this.message.hashHex())
+
+        if (this.morphBytes) {
+            message = hash256(
+                concatBytes(this.morphBytes.toBytes(), this.message.toBytes())
+            )
+        }
+
+        return Program.fromBytes(
+            hash256(concatBytes(fromHex(this.originInfo), message))
+        )
+    }
+}
+export default Announcement
