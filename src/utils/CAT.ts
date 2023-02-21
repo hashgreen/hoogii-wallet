24e3aa6c0d01a2608a6b3bf2c15185dbb052d203
AhWeiHENGuEi <aa9440307@gmail.com>
2023-02-21 17:11:09 +0800

    fix: cat memo repeat bug

--- src/utils/CAT.ts
+++ src/utils/CAT.ts
@@ -107,16 +107,16 @@ export class CAT extends Program {
             })
         }
 
-        const conditionList: Program[] = primaryList.map((primary) => {
+        const conditionList: Program[] = primaryList.map((primary, index) => {
             const additionalMemoList: Program[] = []
-            if (primary.puzzlehash === targetPuzzleHash) {
+
+            if (primary.puzzlehash === targetPuzzleHash && index === 0) {
                 additionalMemoList.push(Program.fromHex(primary.puzzlehash))
-                if (primary.memos?.length) {
-                    primary.memos.forEach((memo) => {
-                        additionalMemoList.push(Program.fromSource(memo))
-                    })
-                }
+                primary?.memos?.forEach((memo) => {
+                    additionalMemoList.push(Program.fromSource(memo))
+                })
             }
+            console.log('additionalMemoList', additionalMemoList)
             return Program.fromList([
                 Program.fromHex(sanitizeHex(ConditionOpcode.CREATE_COIN)),
                 Program.fromHex(primary.puzzlehash),
