b73aad5c99fd65d53223d64ecd474c8cfde5ccd1
AhWeiHENGuEi <aa9440307@gmail.com>
2023-02-23 22:01:09 +0800

    style: json parse style

--- src/popup/components/pushTxInfo.tsx
+++ src/popup/components/pushTxInfo.tsx
@@ -11,7 +11,7 @@ function pushTxInfo({ request }: IPopupPageProps<MethodEnum.REQUEST>) {
             </div>
 
             <div className="bg-box flex flex-col gap-1 px-2 py-3 shrink cursor-pointer rounded-sm ">
-                <div className="text-left text-caption text-primary-100 h-[180px] overflow-y-auto">
+                <div className="text-caption text-primary-100 h-[180px] overflow-y-auto">
                     <JsonView
                         data={request?.data?.params?.spendBundle}
                         shouldInitiallyExpand={() => true}
