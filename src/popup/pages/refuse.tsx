4f0420ffdd2945ccd60b64c5d742ee18d5e58e4e
programmerkenzi <a23g32@gmail.com>
2023-03-16 17:17:31 +0800

    feat: ga event connect_site

--- src/popup/pages/refuse.tsx
+++ src/popup/pages/refuse.tsx
@@ -1,7 +1,9 @@
 import { useNavigate } from 'react-router-dom'
 
+import { sendMeasurement } from '~/api/ga'
 import ConnectSiteInfo from '~/popup/components/connectSiteInfo'
 import { APIError, MethodEnum } from '~/types/extension'
+import { ActionEnum, CategoryEnum, EventEnum } from '~/types/ga'
 
 import { IPopupPageProps } from '../types'
 
@@ -10,22 +12,23 @@ const Refuse = ({
     request,
 }: IPopupPageProps<MethodEnum.ENABLE>) => {
     const navigate = useNavigate()
+
     return (
         <div className="container flex flex-col justify-between w-full h-full py-12">
-            <div className="flex flex-col gap-2 items-center">
+            <div className="flex flex-col items-center gap-2">
                 <ConnectSiteInfo request={request} controller={controller} />
-                <div className="flex gap-2 text-center text-xl">
+                <div className="flex gap-2 text-xl text-center">
                     Connect with Hoogii:
                 </div>
-                <div className="flex gap-2  text-center text-body1 text-primary-100">
+                <div className="flex gap-2 text-center text-body1 text-primary-100">
                     This app would like to:
                 </div>
-                <div className="flex gap-2  text-body1 text-left w-full mt-10">
+                <div className="flex w-full gap-2 mt-10 text-left text-body1">
                     Dapp’s Authorization contents
                 </div>
             </div>
             <div className="flex flex-col w-full">
-                <div className="text-center mb-5 text-body1 text-primary-100">
+                <div className="mb-5 text-center text-body1 text-primary-100">
                     Only connect with sites you trust.
                 </div>
                 <div className="flex justify-between">
@@ -48,12 +51,24 @@ const Refuse = ({
                     </button>
                     <button
                         className="btn btn-CTA_landing  w-[160px] h-[40px] btn-large"
-                        onClick={async () => {
+                        onClick={() => {
                             // await rootStore.walletStore.db.connectedSites.add({
                             //     name: request.origin,
                             //     url: request.origin,
                             // })
                             // controller.checkIsConnectedSite()
+                            sendMeasurement({
+                                events: [
+                                    {
+                                        name: EventEnum.CONNECT_SITE,
+                                        params: {
+                                            category: CategoryEnum.CONNENT_SITE,
+                                            action: ActionEnum.CLICK,
+                                            value: request.origin,
+                                        },
+                                    },
+                                ],
+                            })
                             navigate('/connecting')
                         }}
                     >
