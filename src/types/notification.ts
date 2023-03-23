f09dc84f838f9a8bbab59891faf16eabac22a017
programmerkenzi <a23g32@gmail.com>
2023-03-23 17:32:36 +0800

    chore: move version data to public

--- src/types/notification.ts
+++ src/types/notification.ts
@@ -4,12 +4,11 @@ interface IContnet {
 }
 
 export interface INotification {
-    id: string
     img?: string
+    version?: string
     title: string
     subtitle: string
     contents?: IContnet[]
-    onClose?: () => void
 }
 
 export type NotificationType = 'version' | 'reward'
@@ -23,46 +22,6 @@ export type TxRewardNotification = Pick<
     INotification,
     'img' | 'title' | 'subtitle'
 >
-
-export const versionData: VersionNotification = {
-    title: 'Updates in Hoogii !',
-    subtitle: 'Version 0.0.7',
-    contents: [
-        {
-            title: 'Activity: Farming rewords',
-            description:
-                'Now Hoogii shows Farming rewords in activity! You’ll also get farming rewords reminder by popup and browser notifications.',
-        },
-        {
-            title: 'Browser Notification',
-        },
-        {
-            title: 'Fix error alert that not shows when transaction not be submitted.',
-        },
-        {
-            title: 'Activity: Farming rewords',
-            description:
-                'Now Hoogii shows Farming rewords in activity! You’ll also get farming rewords reminder by popup and browser notifications.',
-        },
-        {
-            title: 'Browser Notification',
-        },
-        {
-            title: 'Fix error alert that not shows when transaction not be submitted.',
-        },
-        {
-            title: 'Activity: Farming rewords',
-            description:
-                'Now Hoogii shows Farming rewords in activity! You’ll also get farming rewords reminder by popup and browser notifications.',
-        },
-        {
-            title: 'Browser Notification',
-        },
-        {
-            title: 'Fix error alert that not shows when transaction not be submitted.',
-        },
-    ],
-}
 
 export const rewardData: TxRewardNotification = {
     img: '/images/icons/tx-status-reward.svg',
