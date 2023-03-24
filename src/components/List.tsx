5a4a9a79a6d6c572ceec26313c8b53b2a5ed5b8b
programmerkenzi <a23g32@gmail.com>
2023-03-24 13:21:01 +0800

    feat: add selectedItemList & logic

---
+++ src/components/List.tsx
@@ -0,0 +1,43 @@
+// src/SelectedAssetList.tsx
+
+import { ICryptocurrency } from '~/types/api'
+interface ISelectedAssetList {
+    assets: ICryptocurrency[]
+    display: boolean
+    onRemove: (id: string) => void
+}
+
+export const SelectedAssetList = ({
+    assets,
+    onRemove,
+    display,
+}: ISelectedAssetList) => {
+    return display ? (
+        <div>
+            <div className=" p-4 w-full bg-primary-100 flex flex-col    h-[300px] rounded-lg overflow-y-scroll ">
+                {assets.map((asset) => (
+                    <div
+                        key={asset.asset_id}
+                        className="flex items-center justify-between w-full h-10 py-2"
+                    >
+                        <div className="flex flex-row w-full gap-2">
+                            <img
+                                src={asset.icon_url}
+                                alt={asset.code}
+                                className="w-6 h-6"
+                            />
+                            <span className=" text-subtitle1 text-[#0E1F4D]">
+                                {asset.code}
+                            </span>
+                        </div>
+                        <img
+                            className="cursor-pointer"
+                            src="/images/icons/close-rounded.svg"
+                            onClick={() => onRemove(asset.asset_id)}
+                        />
+                    </div>
+                ))}
+            </div>
+        </div>
+    ) : null
+}
