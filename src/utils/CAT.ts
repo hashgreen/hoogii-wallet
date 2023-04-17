ad7ed2a3b8f3e1e5ac7bdc42c77a173ae26bf7ea
AhWeiHENGuEi <aa9440307@gmail.com>
2023-04-17 17:27:30 +0800

    fix: error message for cat coin not enough

--- src/utils/CAT.ts
+++ src/utils/CAT.ts
@@ -106,6 +106,12 @@ export class CAT extends Program {
         // move to out
         const coinList = Wallet.selectCoins(spendableCoinList, spendAmount)
 
+        if (coinList.length === 0) {
+            throw new Error(
+                `Not enough CAT(${assetId}) balance(Coin) for this transaction`
+            )
+        }
+
         const sumSpendingValue = coinList.reduce(
             (acc, cur) => acc + cur.amount,
             0n
